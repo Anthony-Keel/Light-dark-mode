db9b366ef8650c93ec7177ffabaa06abc0dbaa0b
Anthony <farmtireguy@gmail.com>
2022-03-28 22:00:48 -0500

    cleaned up code
    used ternary operators to dry code and pare down two function to one

--- script.js
+++ script.js
@@ -13,36 +13,28 @@ function imageMode(color){
     image1.src = `img/undraw_proud_coder_${color}.svg`;
     image2.src = `img/undraw_feeling_proud_${color}.svg`;
     image3.src = `img/undraw_conceptual_idea_${color}.svg`;
-
 }
-// dark mode styles
-function darkMode(){
-    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    textBox.style.backgroundColor = 'rgb(255 255 255 /50%)';
-    toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-    imageMode('dark');
-}
-// light mode styles
+function toggleDarkLightMode(isDark){
 
-function lightMode(){
-    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    textBox.style.backgroundColor = 'rgb(0 0 0  /50%)';
-    toggleIcon.children[0].textContent = 'light Mode';
+    nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 /50%)';
+    textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 /50%)' : 'rgb(0 0 0  /50%)';
+    toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+    isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
     toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    imageMode('light');
+    isDark ? imageMode('dark') : imageMode('light');
 }
+    
 
 // Switch themes Dynamically
 function switchTheme(event){
     if(event.target.checked){
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme','dark');
-        darkMode();
+       toggleDarkLightMode(true);
     }else {
         document.documentElement.setAttribute('data-theme','light');
         localStorage.setItem('theme','light');
-        lightMode();
+        toggleDarkLightMode(false);
     }
 }
 // event listener
@@ -55,6 +47,6 @@ if (currentTheme){
     document.documentElement.setAttribute('data-theme', currentTheme);
     if (currentTheme === 'dark'){
         toggleSwitch.checked = true;
-        darkMode();
+        toggleDarkLightMode(true);
     }
 }
